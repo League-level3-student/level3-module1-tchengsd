a9601d0d3223433df8ba8955021465ecd0ad4f23
league <league@imac-22.attlocal.net>
2019-09-12 19:55:37 -0700

    Hang Man :sunglasses: 

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -16,6 +16,9 @@ public class HangMan implements KeyListener {
 	JPanel panel = new JPanel();
 	JLabel word = new JLabel();
 	JLabel livesLeft = new JLabel();
+	int lives;
+	String gameWord;
+	String currentWord;
 
 	public static void main(String[] args) {
 		new HangMan().playHangman();
@@ -24,38 +27,68 @@ public class HangMan implements KeyListener {
 	void playHangman() {
 		String numberOfWords = JOptionPane.showInputDialog("How many words would you like to guess?");
 		int wordAmt = Integer.parseInt(numberOfWords);
-		int lives = 10 * wordAmt;
-		String s = "";
-		
+		lives = 10 * wordAmt;
+		gameWord = "";
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		word.setText("");
+		livesLeft.setText("Lives remaining: " + lives);
+		frame.add(panel);
+		frame.addKeyListener(this);
+		panel.add(word);
+		panel.add(livesLeft);
 		for (int i = 0; i < wordAmt; i++) {
 			String psh = Utilities.readRandomLineFromFile("dictionary.txt");
 			if (!guessWords.contains(psh)) {
 				guessWords.push(psh);
 			}
 			else {
-				psh = Utilities.readRandomLineFromFile("dictionary.txt");
-				guessWords.push(psh);
+				i--;
 			}
 		}
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		word.setText("");
-		livesLeft.setText("Lives remaining: " + lives);
-		frame.add(panel);
-		panel.add(word);
-		panel.add(livesLeft);
-		String pop = guessWords.pop();
-		for(int i = 0; i < pop.length(); i++) {
-			s+="_";
+		currentWord = guessWords.pop();
+		System.out.println(currentWord);
+		for(int i = 0; i < currentWord.length(); i++) {
+			gameWord+="_";
 		}
-		word.setText(s);
+		word.setText(gameWord);
+		frame.pack();
+		if(gameWord.equals(currentWord) && !guessWords.empty()) {
+			getNewWord();
+		}
+	}
+	
+	void getNewWord() {
+		currentWord = guessWords.pop();
+		for(int i = 0; i < currentWord.length(); i++) {
+			gameWord+="_";
+		}
+		word.setText(gameWord);
+		livesLeft.setText("Lives remaining: " + lives);
 		frame.pack();
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		char c = e.getKeyChar();
+		String str = "";
+		if(gameWord != null) {
+			for(int i = 0; i < currentWord.length(); i++) {
+				if(currentWord.charAt(i) == c) {
+					str+=c;
+				}
+				else {
+					str+=gameWord.charAt(i);
+				}
+			}
+			if(!currentWord.contains(String.valueOf(c))) {
+				lives--;
+			}
+			gameWord = str;
+			word.setText(gameWord);
+			livesLeft.setText("Lives remaining: " + lives);
+		}
 	}
 
 	@Override
