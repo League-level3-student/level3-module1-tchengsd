91845985c0bbe65ab055148ed3de03f0c36fa8aa
league <league@imac-5.attlocal.net>
2019-09-26 19:57:39 -0700

    Sting Integer Pair, Intro to Hash Maps

--- src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
+++ src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
@@ -4,17 +4,26 @@ import java.util.HashMap;
 
 public class _01_IntroToHashMaps {
 	public static void main(String[] args) {
-		//1. Create a HashMap called roots with Integers for the keys and Doubles for the values.
+		// 1. Create a HashMap called roots with Integers for the keys and Doubles for
+		// the values.
+		HashMap<Integer, Double> roots = new HashMap<Integer, Double>();
+		// 2. Using a for-loop, add 500 entries to your HashMap. The key entry will be
+		// the current iteration of the loop (i). The value entry will be the square root of
+		// i.
+		for(int i = 0; i < 500; i++) {
+			roots.put(i, Math.sqrt(i));
+		}
 
-		//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
-		//   current iteration of the loop (i). The value entry will be the square root of i.
+		// 3. Iterate through all the entries in your HashMap displaying the keys with
+		// their respective
+		// square roots (values). Use the following format.
+		for(int i = 0; i < 500; i++) {
+			System.out.println("The square root of " + i + " is " + roots.get(i));
+		}
 
-		//3. Iterate through all the entries in your HashMap displaying the keys with their respective
-		//   square roots (values). Use the following format.
-
-		//   The square Root of 0 is 0.0
-		//   The square Root of 1 is 1.0
-		//   The square Root of 2 is 1.4142135623730951
-		//   The square Root of 3 is 1.7320508075688772
+		// The square Root of 0 is 0.0
+		// The square Root of 1 is 1.0
+		// The square Root of 2 is 1.4142135623730951
+		// The square Root of 3 is 1.7320508075688772
 	}
 }
